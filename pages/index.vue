e577508854c367e6f0d166657b56cca3b37bd8ec
lerua <chantrya@gmail.com>
2023-07-16 14:41:18 +0800

    Filter out passed events

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
     <div>
         <h2 class="font-bold">Pont Chaban</h2>
         <div>
-            <div v-for="r in records.records" class="p-1">
+            <div v-for="r in futureRecords" class="p-1">
                 <div class="card flex justify-between">
                     <div>
                     <i class="material-icons mr-2">calendar_today</i>{{ formatDate(r.fields.date_passage) }}
@@ -35,6 +35,19 @@
     //  fetch the records
     const { data: records } = await useFetch('https://opendata.bordeaux-metropole.fr/api/records/1.0/search/?dataset=previsions_pont_chaban&q=&rows=50')
     
+    // Remove the events from the past
+    const today = new Date();
+    today.setHours(0,0,0,0); // Set time to 00:00:00
+
+    const futureRecords = computed(() => {
+        return records.value.records.filter((r) => {
+            const recordDate = new Date(r.fields.date_passage);
+            recordDate.setHours(0,0,0,0);
+            return recordDate >= today;
+        });
+    });
+
+
     // method to format date
     const formatDate = (dateString) => {
         const options = { weekday: 'long', month: 'long', day: 'numeric' } // Required format
