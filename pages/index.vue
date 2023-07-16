bfbde6f9e2783d48f4f4979803980b2036bb888d
lerua <chantrya@gmail.com>
2023-07-16 13:28:13 +0800

    Add the Add to calendar button

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,17 @@
                     {{ formatDate(r.fields.date_passage) }}
                     <p>De {{ r.fields.fermeture_a_la_circulation }} à {{ r.fields.re_ouverture_a_la_circulation }}</p>
                     <p>Bateau : {{ r.fields.bateau }}</p>
+                    {{r.fields.date_passage}}
+                    <add-to-calendar-button
+                        name="Fermeture pont Chaban"
+                        options="'Apple','Google','Microsoft365','ical'"
+                        location="Pont Chaban delmas, Bordeaux, France"
+                        :startDate="r.fields.date_passage"
+                        :startTime="r.fields.fermeture_a_la_circulation"
+                        :endTime="r.fields.re_ouverture_a_la_circulation"
+                        :description="'Passage du bateau ' + r.fields.bateau"
+                        timeZone="Europe/Paris"
+                        ></add-to-calendar-button>
                 </div>
             </div>
       </div>
@@ -15,6 +26,8 @@
 
 <script setup>
     import { ref } from 'vue'
+    import 'add-to-calendar-button';
+
     //  fetch the records
     const { data: records } = await useFetch('https://opendata.bordeaux-metropole.fr/api/records/1.0/search/?dataset=previsions_pont_chaban&q=&rows=20')
     
@@ -24,6 +37,12 @@
         const date = new Date(dateString)
         return date.toLocaleDateString(undefined, options)
     }
+    // Format date for calendar
+    const formatDateForCalendar = (dateString) => {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' } // Required format
+        const date = new Date(dateString)
+        return date.toLocaleDateString(undefined, options)
+}
 </script>
 
 <style scoped>
