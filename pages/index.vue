4b8e237ee0975e3d14959788226adc6658c4d9a5
lerua <chantrya@gmail.com>
2023-07-16 13:50:35 +0800

    alignement du bouton dans les cards

--- pages/index.vue
+++ pages/index.vue
@@ -3,21 +3,25 @@
         <h2 class="font-bold">Pont Chaban</h2>
         <div>
             <div v-for="r in records.records" class="p-1">
-                <div class="card">
-                    {{ formatDate(r.fields.date_passage) }}
-                    <p>De {{ r.fields.fermeture_a_la_circulation }} à {{ r.fields.re_ouverture_a_la_circulation }}</p>
-                    <p>Bateau : {{ r.fields.bateau }}</p>
-                    {{r.fields.date_passage}}
-                    <add-to-calendar-button
+                <div class="card flex justify-between">
+                    <div>
+                    <i class="material-icons mr-2">calendar_today</i>{{ formatDate(r.fields.date_passage) }}
+                    <p><i class="material-icons mr-2">schedule</i>De {{ r.fields.fermeture_a_la_circulation }} à {{ r.fields.re_ouverture_a_la_circulation }}</p>
+                    <p><i class="material-icons mr-2">directions_boat</i>Bateau : {{ r.fields.bateau }}</p>
+                </div>
+ <!-- TODO : Handle the case when endTime is not same day as startTime -->
+                    <add-to-calendar-button class=""
                         name="Fermeture pont Chaban"
-                        options="'Apple','Google','Microsoft365','ical'"
+                        options="'Apple','Google','Microsoft365','ical','Outlook.com'"
                         location="Pont Chaban delmas, Bordeaux, France"
                         :startDate="r.fields.date_passage"
                         :startTime="r.fields.fermeture_a_la_circulation"
                         :endTime="r.fields.re_ouverture_a_la_circulation"
                         :description="'Passage du bateau ' + r.fields.bateau"
                         timeZone="Europe/Paris"
-                        ></add-to-calendar-button>
+                        label="Ajouter à mon calendrier"
+                        >
+                    </add-to-calendar-button>
                 </div>
             </div>
       </div>
@@ -29,7 +33,7 @@
     import 'add-to-calendar-button';
 
     //  fetch the records
-    const { data: records } = await useFetch('https://opendata.bordeaux-metropole.fr/api/records/1.0/search/?dataset=previsions_pont_chaban&q=&rows=20')
+    const { data: records } = await useFetch('https://opendata.bordeaux-metropole.fr/api/records/1.0/search/?dataset=previsions_pont_chaban&q=&rows=50')
     
     // method to format date
     const formatDate = (dateString) => {
